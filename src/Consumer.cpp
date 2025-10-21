d4197f8d761920b301db9aede774d5da8b0ab3ef
Matthieu Dorier <matthieu.dorier@gmail.com>
2025-10-21 14:57:27 +0100

    Initial commit

---
+++ src/Consumer.cpp
@@ -0,0 +1,66 @@
+#include "AAA/Consumer.hpp"
+#include "AAA/Driver.hpp"
+#include "AAA/TopicHandle.hpp"
+#include "AAA/ThreadPool.hpp"
+
+#include <condition_variable>
+
+namespace BBB {
+
+CCCConsumer::CCCConsumer(
+        std::string name,
+        diaspora::BatchSize batch_size,
+        diaspora::MaxNumBatches max_num_batches,
+        std::shared_ptr<CCCThreadPool> thread_pool,
+        std::shared_ptr<CCCTopicHandle> topic,
+        diaspora::DataAllocator data_allocator,
+        diaspora::DataSelector data_selector)
+: m_name{std::move(name)}
+, m_batch_size(batch_size)
+, m_max_num_batches(max_num_batches)
+, m_thread_pool(std::move(thread_pool))
+, m_topic(std::move(topic))
+, m_data_allocator{std::move(data_allocator)}
+, m_data_selector{std::move(data_selector)}
+{}
+
+std::shared_ptr<diaspora::TopicHandleInterface> CCCConsumer::topic() const {
+      return m_topic;
+}
+
+void CCCConsumer::unsubscribe() {}
+
+void CCCConsumer::process(
+        diaspora::EventProcessor processor,
+        std::shared_ptr<diaspora::ThreadPoolInterface> threadPool,
+        diaspora::NumEvents maxEvents) {
+    if(!threadPool) threadPool = m_topic->driver()->defaultThreadPool();
+    size_t                  pending_events = 0;
+    std::mutex              pending_mutex;
+    std::condition_variable pending_cv;
+    try {
+        for(size_t i = 0; i < maxEvents.value; ++i) {
+            auto event = pull().wait();
+            {
+                std::unique_lock lock{pending_mutex};
+                pending_events += 1;
+            }
+            threadPool->pushWork([&, event=std::move(event)]() {
+                    processor(event);
+                    std::unique_lock lock{pending_mutex};
+                    pending_events -= 1;
+                    if(pending_events == 0)
+                    pending_cv.notify_all();
+                    });
+        }
+    } catch(const diaspora::StopEventProcessor&) {}
+    std::unique_lock lock{pending_mutex};
+    while(pending_events) pending_cv.wait(lock);
+}
+
+diaspora::Future<diaspora::Event> CCCConsumer::pull() {
+    // TODO
+    throw diaspora::Exception{"CCCConsumer::pull not implemented"};
+}
+
+}
---
+++ src/FutureState.hpp
@@ -0,0 +1,49 @@
+#ifndef DDD_FUTURE_STATE_H
+#define DDD_FUTURE_STATE_H
+
+#include <diaspora/Exception.hpp>
+#include <mutex>
+#include <condition_variable>
+#include <variant>
+
+namespace BBB {
+
+template<typename T>
+struct FutureState {
+
+    std::mutex                           mutex;
+    std::condition_variable              cv;
+    std::variant<T, diaspora::Exception> value;
+    bool                              is_set = false;
+
+    template<typename U>
+    void set(U u) {
+        if(is_set) throw diaspora::Exception{"Promise already set"};
+        {
+            std::unique_lock lock{mutex};
+            value = std::move(u);
+            is_set = true;
+        }
+        cv.notify_all();
+    }
+
+    T wait() {
+        std::unique_lock lock{mutex};
+        while(!is_set) {
+            cv.wait(lock);
+        }
+        if(std::holds_alternative<T>(value))
+            return std::get<T>(value);
+        else
+            throw std::get<diaspora::Exception>(value);
+    }
+
+    bool test() {
+        std::unique_lock lock{mutex};
+        return is_set;
+    }
+};
+
+}
+
+#endif
