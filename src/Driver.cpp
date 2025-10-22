39cc3588901c254b90f1bdc92f6afce705a96a75
Matthieu Dorier <mdorier@anl.gov>
2025-10-22 09:56:45 +0100

    refactor: replace librdkafka with libcurl and Kafka REST API for topic creation

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -14,8 +14,6 @@ void OctopusDriver::createTopic(std::string_view name,
     if(!options.json().is_object())
         throw diaspora::Exception{"Invalid config passed to OctopusDriver::createTopic: should be an object"};
 
-    KafkaConf kconf{m_options};
-
     // Get the options
     size_t num_partitions = 1;
     if(options.json().contains("num_partitions")) {
@@ -33,65 +31,18 @@ void OctopusDriver::createTopic(std::string_view name,
             throw diaspora::Exception{"Invalid value for \"replication_factor\" option"};
     }
 
-    char errstr[512];
-    auto rk = std::shared_ptr<rd_kafka_t>{
-        rd_kafka_new(RD_KAFKA_PRODUCER, kconf, errstr, sizeof(errstr)),
-        rd_kafka_destroy};
-    if (!rk) throw diaspora::Exception{"Could not create rd_kafka_t instance: " + std::string{errstr}};
+    // TODO use libcurl and the Kafka REST API to create a topic with the given name, number of partitions and replication_factor. The m_options field of the OctopusDriver should contain a "bootstrap.servers" key with the list of Kafka servers.
 
-    // Create the NewTopic object
-    auto new_topic = std::shared_ptr<rd_kafka_NewTopic_s>{
-        rd_kafka_NewTopic_new(name.data(), num_partitions, replication_factor, errstr, sizeof(errstr)),
-        rd_kafka_NewTopic_destroy};
-    if (!new_topic) throw diaspora::Exception{"Failed to create NewTopic object: " + std::string{errstr}};
+    // TODO the function will also create a topic named "_<name>_info_" (with 1 partition and a replication factor identical to the above) and store the validator, selector, and serializer. These can be stored by converting them into a Metadata object and dump this metadata into a string from its JSON format (e.g. serializer.metadata().json().dump()).
+}
 
-    // Create an admin options object
-    auto admin_options = std::shared_ptr<rd_kafka_AdminOptions_s>{
-        rd_kafka_AdminOptions_new(rk.get(), RD_KAFKA_ADMIN_OP_CREATETOPICS),
-        rd_kafka_AdminOptions_destroy};
-    if (!admin_options) throw diaspora::Exception{"Failed to create rd_kafka_AdminOptions_t"};
-
-    // Create a queue for the result of the operation
-    auto queue = std::shared_ptr<rd_kafka_queue_t>{
-        rd_kafka_queue_new(rk.get()),
-        rd_kafka_queue_destroy};
-
-    // Initiate the topic creation
-    rd_kafka_NewTopic_t *new_topics[] = {new_topic.get()};
-    rd_kafka_CreateTopics(rk.get(), new_topics, 1, admin_options.get(), queue.get());
-
-    // Wait for the result for up to 10 seconds
-    auto event = std::shared_ptr<rd_kafka_event_t>{
-        rd_kafka_queue_poll(queue.get(), 10000),
-        rd_kafka_event_destroy};
-    if (!event) throw diaspora::Exception{"Timed out waiting for CreateTopics result"};
-
-    // Check if the event type is CreateTopics result
-    if (rd_kafka_event_type(event.get()) != RD_KAFKA_EVENT_CREATETOPICS_RESULT)
-        throw diaspora::Exception{"Unexpected event type when waiting for CreateTopics"};
-
-    // Extract the result from the event
-    auto result = rd_kafka_event_CreateTopics_result(event.get());
-    size_t topic_count;
-    auto topics_result = rd_kafka_CreateTopics_result_topics(result, &topic_count);
-
-    // Check the results for errors
-    for (size_t i = 0; i < topic_count; i++) {
-        const rd_kafka_topic_result_t *topic_result = topics_result[i];
-        if (rd_kafka_topic_result_error(topic_result) != RD_KAFKA_RESP_ERR_NO_ERROR) {
-            throw diaspora::Exception{"Failed to create topic: "
-                + std::string{rd_kafka_err2str(rd_kafka_topic_result_error(topic_result))}};
-        }
-    }
+bool OctopusDriver::topicExists(std::string_view name) const {
+    // TODO check that the topic with the given name exists, using the Kafka REST API and libcurl.
 }
 
 std::shared_ptr<diaspora::TopicHandleInterface> OctopusDriver::openTopic(
         std::string_view name) const {
-    // TODO
-}
-
-bool OctopusDriver::topicExists(std::string_view name) const {
-    // TODO
+    // TODO open the topic with the given name. This is done by opening the "_<name>_info_" topic usig librdkafka and reading its content to get the validator, selector, and serializer back.
 }
 
 std::shared_ptr<diaspora::ThreadPoolInterface> OctopusDriver::defaultThreadPool() const {
