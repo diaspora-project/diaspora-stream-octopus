df789edd692d87a3c71a76f0c5e5c0d85df52464
Matthieu Dorier <mdorier@anl.gov>
2025-10-31 11:07:39 +0000

    adapted to change to Driver constructor

--- tests/create-topic-for-benchmark.py
+++ tests/create-topic-for-benchmark.py
@@ -1,4 +1,4 @@
 from diaspora_stream.api import Driver
 
-driver = Driver.new("octopus", options={"bootstrap.servers":"localhost:9092"})
+driver = Driver(backend="octopus", options={"bootstrap.servers":"localhost:9092"})
 driver.create_topic("my_topic", options={"replication_factor":1})
